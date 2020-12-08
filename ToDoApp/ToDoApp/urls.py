929bd65f975b1913b6b0870b2c57c29afc308b55
Manan Gyanchandani <manan.co.in@gmail.com>
2020-12-08 23:48:55 +0530

    made changes in styling

--- ToDoApp/ToDoApp/urls.py
+++ ToDoApp/ToDoApp/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from todo.views import todoView, addTodo, deleteTodo
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', todoView),   
     path('todo/', todoView),   
     path('addTodo/',addTodo),
     path('deleteTodo/<int:todo_id>/',deleteTodo),

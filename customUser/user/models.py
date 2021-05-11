0dd46de6bb21f4df194c1678473cc75e52ed788a
manan2110 <manan.co.in@gmail.com>
2021-05-12 04:50:35 +0530

    creating a custom user (AbstractUser) in django

---
+++ customUser/user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    Roles = (
+        ('A','Agent'),
+        ('O','Office'),
+    )
+    role = models.CharField(max_length = 1 , choices = Roles)

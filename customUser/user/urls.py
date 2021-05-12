d6fe4825b0d5171c120baf574fe109e6d6a17610
manan2110 <manan.co.in@gmail.com>
2021-05-12 18:44:04 +0530

    Frontend for custom User created

--- customUser/user/urls.py
+++ customUser/user/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('user.urls')),
+    path('', views.index, name="home"),
+    path('login', views.loginUser, name="login"),
+    path('logout', views.logoutUser, name="logout"),
 ]

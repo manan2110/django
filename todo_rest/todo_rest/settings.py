3df92c436692e4c04a221da50c9c796c7b1cab45
manan2110 <manan.co.in@gmail.com>
2021-06-06 02:10:57 +0530

    Added js for building the list

--- todo_rest/todo_rest/settings.py
+++ todo_rest/todo_rest/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
-    'frontend',
+    'api.apps.ApiConfig',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [

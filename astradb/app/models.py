64169bd00367cbd03a5c81c03577c6d8fe005098
manan2110 <manan.co.in@gmail.com>
2021-09-01 00:26:21 +0530

    using astradb

---
+++ astradb/app/models.py
@@ -0,0 +1,12 @@
+import uuid
+from cassandra.cqlengine import columns
+from django_cassandra_engine.models import DjangoCassandraModel
+from cassandra.cqlengine import models
+
+
+class ExampleModel(models.Model):
+    stealth_options = ("option_name", ...)
+    example_id = columns.UUID(primary_key=True, default=uuid.uuid4)
+    example_type = columns.Integer(index=True)
+    created_at = columns.DateTime()
+    description = columns.Text(required=False)
